3eacffdb5c6d8ffc89c231722c08b69cd2421ad4
exshak <sbijabhai@gmail.com>
2020-11-10 11:38:43 -0500

    updated setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# brew bundle --file=~/.brewfile
+# sudo xcodebuild -license accept
+# compaudit | xargs chmod g-w,o-w
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# sudo gem install colorls
+# $(brew --prefix)/opt/fzf/install
+# bash .macos

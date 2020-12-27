3261c2f5d07dd8deb672de1a3c11aa35ea366d78
exshak <sbijabhai@gmail.com>
2020-12-27 00:00:00 -0500

    removed tpm step

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 # brew bundle --file=~/.brewfile
 # sudo xcodebuild -license accept
 # compaudit | xargs chmod g-w,o-w
-# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # sudo gem install colorls
 # $(brew --prefix)/opt/fzf/install
 # bash .macos

a739a96e02e823a24557e72801b036896850d5e2
exshak <sbijabhai@gmail.com>
2021-03-02 14:35:58 -0500

    dotfiles: updated setup

--- .config/dotfiles/setup.sh
+++ .config/dotfiles/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# brew bundle --file=~/Brewfile
+
+brew bundle --file=~/Brewfile
 # sudo xcodebuild -license accept
+sudo softwareupdate --install-rosetta --agree-to-license
 # compaudit | xargs chmod g-w,o-w
-# sudo gem install colorls
-# $(brew --prefix)/opt/fzf/install
-# bash .macos
+gem install colorls
+$(brew --prefix)/opt/fzf/install
+bash ~/.macos
+echo "gitdir: $HOME/.dotfiles" > ~/.git
